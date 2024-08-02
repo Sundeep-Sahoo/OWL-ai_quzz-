271de1bd793af1b5d9caddfcec55649fded25dc7
om sundeep sahoo <132585513+Sundeep-Sahoo@users.noreply.github.com>
2024-08-02 21:40:01 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,5 @@ export default function Home() {
     </footer>
     </div>
 
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-center">
-//       <h1 className="text-6xl font-bold">Hello World👋</h1>
-//     </main>
-//   )
-// }
+  )
+}
