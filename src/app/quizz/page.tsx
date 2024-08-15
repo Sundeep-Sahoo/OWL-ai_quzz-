6506236b91a8c5925637630d59a6c6c1539af3ab
Debasish0024 <debasishswain2402@gmail.com>
2024-08-15 22:12:27 +0530

    35 min border implementation, correct % incorrect msg

--- src/app/quizz/page.tsx
+++ src/app/quizz/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import ProgressBar from "@/components/progressBar";
+import { ChevronLeft, X } from "lucide-react";
+import ResultCard from "./ResultCard";
 
 export default function Home() {
-  const [started , setStarted]=useState(false);
-
-  const [currentQuestion,setCurrentQuestion]=useState(0);
+  const [started , setStarted]=useState<boolean>(false);
+  const [currentQuestion,setCurrentQuestion]=useState<number>(0);
+  const [score, setScore] =useState<number>(0);
+  const [selectedAnswer, setSelectedAnswer] =useState<number | null>(null);
+  const [isCorrect,setIsCorrect] = useState<number | null>(null);
   const questions = [
     {
       questionText: "What is React?",
@@ -45,12 +50,31 @@ export default function Home() {
     if (currentQuestion < questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
     }  
+
+    setSelectedAnswer(null);
+    setIsCorrect(null);
   }
+
+  const handleAnswer = (answer) => {
+    setSelectedAnswer(answer.id);
+    const isCurrentCorrect = answer.isCorrect;
+    if (isCurrentCorrect){
+      setScore(score + 1);
+    }
+    setIsCorrect(isCurrentCorrect);
+  }
+
 
   return (
     <div className="flex flex-col flex-1">
-    <main className="flex   
-    justify-center  flex-1">
+      <div className="position-sticky top-0 z-10 shadow-md py-4 w-full">
+        <header className="grid grid-cols-[auto,1fr,auto] grid-flow-col items-center justify-betwwen py-2 gap-2">
+          <Button size="icon" variant="outline"><ChevronLeft/></Button>
+          <ProgressBar value={(currentQuestion/questions.length)*100}/>
+          <Button size="icon" variant="outline"><X/></Button>
+        </header>
+      </div>
+    <main className="flex justify-center  flex-1">
       {!started ? <h1 className="text-3xl font-bold">Welcome to the quizz page👋</h1>:(
         <div>
           <h2 className="text-3xl font-bold">{questions[currentQuestion].questionText}</h2>
@@ -58,7 +82,7 @@ export default function Home() {
               {
                 questions[currentQuestion].answers.map(answer => {
                   return (
-                    <Button key={answer.id} variant={"secondary"}>{answer.answerText}</Button>
+                    <Button key={answer.id} variant={"secondary"}  onClick={() => handleAnswer(answer)}>{answer.answerText}</Button>
                   )
                 })
               }
@@ -67,6 +91,8 @@ export default function Home() {
       )}
     </main>
     <footer className="footer pb-9 px-6 relative mb-0">
+      <ResultCard isCorrect= {isCorrect}  correctAnswer =
+      {questions[currentQuestion].answers.find(answer => answer.isCorrect === true)?.answerText} />
       <Button onClick={handleNext}>{!started ? 'start' : 'Next'}</Button>
     </footer>
     </div>
